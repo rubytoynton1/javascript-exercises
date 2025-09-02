4f186413dc976f00c03f0daed9069af522de9018
Ruby <rubytoynton@gmail.com>
2025-09-02 11:13:48 +0100

    completed 06 repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let newStr = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i=0 ; i < num ; i++) {
+        newStr += string;
+    }
+    return newStr
 };
 
 // Do not edit below this line
