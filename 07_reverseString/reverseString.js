6b8bf59afd52af726f563cc99db3417a19ba4402
Ruby <rubytoynton@gmail.com>
2025-09-02 11:35:37 +0100

    completed 07 reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("");
+    let reverseArr = splitStr.reverse();
+    let joinArr = reverseArr.join("");
+    return joinArr;
 };
 
 // Do not edit below this line
