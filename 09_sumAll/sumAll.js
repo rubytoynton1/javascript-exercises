6ed2102cccceea691a1e74bc5a5bc304d49f8f5b
Ruby <rubytoynton@gmail.com>
2025-09-02 12:23:20 +0100

    completed 09 sumall

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    var finalSum = 0;
+    if ((a < 0) || (b<0)) return "ERROR";
+    if ((!Number.isInteger(a)) || (!Number.isInteger(b))) return "ERROR";
+    
+    if (a<b) {
+        for (let i = a; a <= b ; i++) {
+            finalSum += a;
+            a++;
+        }}
+    else {
+        for (let i = b; b <= a ; i++) {
+            finalSum += b;
+            b++;
+    }}
+    return finalSum;
 };
 
 // Do not edit below this line
