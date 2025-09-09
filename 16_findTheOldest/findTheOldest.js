5d3801328e52836fb615f4aae672db86b627d29d
Ruby <rubytoynton@gmail.com>
2025-09-09 17:50:56 +0100

    completed 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = {age:0};
+    arr.forEach(item => {
+        if (!Object.hasOwn(item,"yearOfDeath")) {
+            let date = new Date().getFullYear()
+            item.age = (date - item.yearOfBirth)
+        } else {
+            item.age = item.yearOfDeath - item.yearOfBirth;
+        }
+        if (item.age > (oldest.age)) {
+            oldest = item;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
