e9a929e4003e1b9b57ee4fbbf30498c49d2ecf0a
Ruby <rubytoynton@gmail.com>
2025-09-09 12:40:23 +0100

    completed 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[^\w_]|_/g, '').toLowerCase().split("");
+    let x = (newStr.length - 1);
+    for (i=0 ; i < (newStr.length /2) ; i++) {
+        if (newStr[i] != newStr[x]){
+            return false;
+        }
+        x--;
+    }
+    return true;
+    
+}
 // Do not edit below this line
 module.exports = palindromes;
