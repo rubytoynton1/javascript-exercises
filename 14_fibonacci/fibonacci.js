f5a43ec474376da4180a5e9adbcb83140d73ece0
Ruby <rubytoynton@gmail.com>
2025-09-09 17:27:35 +0100

    14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    num = parseInt(num);
+    let first = 0;
+    let second = 1;
+    let result = 0;
+    if (num == 1) return second;
+    for (i=0; i < num-1 ; i++){
+        result = first + second;
+        first = second;
+        second = result;
+    }
+    return result;
 };
 
 // Do not edit below this line
