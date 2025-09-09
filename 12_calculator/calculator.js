cb5336a676c05be4c1db4755f069598f0d49330c
Ruby <rubytoynton@gmail.com>
2025-09-09 12:01:56 +0100

    12 calculator done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = arr.reduce((sum, number) => sum + number, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((multiply, number) => multiply * number);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	current = x;
+  for (i=current-1; i > 0; i--) {
+    current = (current * i)
+  }
+  return current;
 };
 
 // Do not edit below this line
